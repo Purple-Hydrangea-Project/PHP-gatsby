ad2ff970f9e32216f6dee792c17fa45ea823a16d
isabiiil <isabelabonitalla@gmail.com>
2020-08-21 12:01:41 -0400

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,4 @@
+import React from "react"
+import "./Button.scss"
+
+// component goes here
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Button from "./Button"
+import Header from "./Header"
+import Landing from "./Landing"
+import Scroller from "./Scroller"
+
+export {
+    Button,
+    Header,
+    Landing,
+    Scroller
+}
