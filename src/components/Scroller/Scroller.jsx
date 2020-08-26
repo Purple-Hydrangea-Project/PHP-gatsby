d5264d0e51c1aab936d7b2c4eb3ce9343999900c
Anna Zhang <anna.zhang@yale.edu>
2020-08-26 14:29:49 -0700

    made scroller component with css

---
+++ src/components/Scroller/Scroller.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./Scroller.css"
+
+const Scroller = () => (
+  <div class="arrow">
+    <span></span>
+    <span></span>
+    <span></span>
+  </div>
+)
+export default Scroller
