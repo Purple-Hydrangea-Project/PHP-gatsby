953edb02974193bba32aa7ae2963d51bdf702979
Anna Zhang <annazhang2002@gmail.com>
2020-09-01 13:39:41 -0700

    created footer

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Button from "./Button"
 import Header from "./Header"
 import Landing from "./Landing"
 import Scroller from "./Scroller"
+import Footer from "./Footer"
 
 export {
     Button,
     Header,
     Landing,
-    Scroller
+    Scroller,
+    Footer,
 }
