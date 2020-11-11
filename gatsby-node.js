3db07162f3c0c234e9026ad41499936a194d4fda
isabiiil <isabelabonitalla@gmail.com>
2020-11-10 19:32:04 -0500

    finished responsiveness; added contentful

---
+++ gatsby-node.js
@@ -0,0 +1,25 @@
+const path = require("path")
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const response = await graphql(`
+    query {
+      allContentfulBlogPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  response.data.allContentfulBlogPost.edges.forEach(edge => {
+    createPage({
+      path: `/blog/${edge.node.slug}`,
+      component: path.resolve("./src/templates/blog-post.js"),
+      context: {
+        slug: edge.node.slug,
+      },
+    })
+  })
+}
--- src/components/ComposeClassName/composeClassName.js
+++
@@ -1,8 +0,0 @@
-const composeClassName = classes => {
-  const theClasses = classes.filter(className => {
-    return !!className
-  })
-  return theClasses.join(" ").trim()
-}
-
-export default composeClassName
