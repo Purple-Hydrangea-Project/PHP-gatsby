5fc2a65acd0cf44f3a933c9c24db76fa911fb333
Ella <exu6056@gmail.com>
2020-12-21 19:50:29 -0800

    quote box

---
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './QuoteBox.scss'
+
+import Quote from '../../../static/QuoteBox.svg'
+
+const QuoteBox = () => (
+        <div className="quote">
+            <img src={Quote}/>
+        </div>
+)
+export default QuoteBox
