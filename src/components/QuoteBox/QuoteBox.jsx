29f02dea018a6e4aee14a88064537c928c9e4219
Ella <exu6056@gmail.com>
2020-12-22 16:43:21 -0800

    not responsive

--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -5,7 +5,7 @@ import Quote from '../../../static/QuoteBox.svg'
 
 const QuoteBox = () => (
         <div className="quote">
-            <img src={Quote}/>
+            <img src={Quote} alt="quote" />
         </div>
 )
 export default QuoteBox
