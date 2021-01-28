ea4c2bc8d247ad45a3caea71482c697663b4ac6d
Ella <exu6056@gmail.com>
2021-01-27 21:37:22 -0800

    add carousel

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,35 +1,10 @@
 import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
 import "../components/main.scss";
-import { Footer, Landing, Button, Card, BlogPage, BlogCard, Counter, TextCard, Feature, Accordion, Navbar, QuoteBox, VineBlurb, Countdown, BubbleButtons } from '../components';
-import Logo from '../../static/circle_logo.png';
+import { Footer, Landing, Counter, Navbar, VineBlurb, Countdown, Carousel } from '../components';
 import Vine from '../../static/vineblurb.png';
 
 
 export default function Home() {
-
-  const data = useStaticQuery(
-    graphql`
-      query {
-        allContentfulTeamMember {
-          edges {
-            node {
-              blurb {
-                blurb
-              }
-              name
-              title
-              image {
-                file {
-                  url
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  )
 
   const counterValues = [
     {
@@ -46,19 +21,17 @@ export default function Home() {
     }
   ]
 
-  const events = [
+  const slide = [
     {
-      name: 'Lecture: Gender and Pay Gap',
-      date: 19,
-      location: 'San Francisco',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum eget quam lectus morbi euismod pellentesque neque, orci magna. Ultrices netus orci facilisis aliquet pulvinar morbi diam.'
+      img: 'https://ichef.bbci.co.uk/news/1024/cpsprodpb/151AB/production/_111434468_gettyimages-1143489763.jpg',
+      num:0,
+      text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum mi ante. Suspendisse vel sem et quam sagittis efficitur non quis tortor. Praesent luctus lorem at leo commodo interdum. Cras non turpis nec enim rutrum vehicula. Praesent malesuada nibh nec hendrerit tempus. Vestibulum at urna finibus, blandit purus quis, consequat tellus. Nullam tincidunt bibendum lectus. Suspendisse arcu nunc, rutrum a mauris eget, mollis sagittis justo."
     },
     {
-      name: 'Lecture: Gender and Pay Gap',
-      date: 19,
-      location: 'San Francisco',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsum eget quam lectus morbi euismod pellentesque neque, orci magna. Ultrices netus orci facilisis aliquet pulvinar morbi diam.'
-    },
+      img: 'https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/cat_relaxing_on_patio_other/1800x1200_cat_relaxing_on_patio_other.jpg',
+      num:1,
+      text:"Phasellus ipsum metus, porttitor quis nunc eu, posuere suscipit enim. Nam mollis lorem dolor. In hac habitasse platea dictumst. Maecenas at pretium ante. Mauris hendrerit sollicitudin mauris eget imperdiet. Vestibulum sit amet magna vitae arcu sodales consectetur. Proin tellus magna, condimentum eget egestas eu, interdum in justo. Nullam accumsan, sem eu suscipit dictum, odio massa vehicula est, a finibus mi nisi vitae massa. Integer lobortis eu lorem quis lobortis."
+    }
   ]
 
   return (
@@ -66,13 +39,11 @@ export default function Home() {
       <Navbar />
       <Landing />
       <VineBlurb img={Vine} text="This is a blurb" />
-      
+      <Carousel events={slide} />
       <Counter values={counterValues} duration={2000} />
-      
       <div className="section row">
         <Countdown />
       </div>
-      
       <Footer />
 
     </div>
