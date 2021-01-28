7e5e1f251db0d60cb012a0cb5a7a91661b3c14ac
isabiiil <isabelabonitalla@gmail.com>
2021-01-27 22:57:13 -0500

    countdown styles and index.js imports

--- src/components/BlogPage/BlogPage.jsx
+++ src/components/BlogPage/BlogPage.jsx
@@ -5,27 +5,27 @@ import { Socials, Button } from '../index';
 import Heart from '../../../static/heart-thin.svg'
 
 export default class BlogPage extends React.Component {
-    render() {
+  render() {
       //const {author, date, blogtext, blogtitle, ...props} = this.props
     return(
       <div class="blogpage">
         <div class="green-box">
-            <div class="post">
-                <div class="post-content">
-                    <h6>date</h6>
-                    <h1>Blog Title</h1>
-                    <h4>author</h4>
-                    <p>the blog...</p>
-                </div>
-                <div class="post-footer">
-                <div class="socials">
-                    <Socials />
-                </div>
-                <div class="likes"><img src={Heart}/></div>
-                </div>
+          <div class="post">
+            <div class="post-content">
+              <h6>date</h6>
+              <h1>Blog Title</h1>
+              <h4>author</h4>
+              <p>the blog...</p>
             </div>
+            <div class="post-footer">
+              <div class="socials">
+                <Socials />
+              </div>
+              <div class="likes"><img src={Heart}/></div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
     )
   }
 }
