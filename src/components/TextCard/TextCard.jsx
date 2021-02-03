1d55981e90f97c71e5e38cce7c2f237451297fb6
Anna Zhang <annazhang2002@gmail.com>
2021-02-02 18:35:02 -0800

    added about page

--- src/components/TextCard/TextCard.jsx
+++ src/components/TextCard/TextCard.jsx
@@ -3,36 +3,36 @@ import React from "react"
 import "./TextCard.scss"
 
 export default class TextCard extends React.Component {
-    render() {
-    const {alignment, text, img, heading, ...props} = this.props
-    if(alignment == "left"){
-        return(
-            <div className="TextCard">
-                <div className="textcard">
-                <div className="lefttext text">
-                <h1>{heading}</h1>
-                <p>{text}</p>
-                </div>
-                  <img src={img}/>
-                </div>
+  render() {
+    const { alignment, text, img, heading, ...props } = this.props
+    if (alignment == "left") {
+      return (
+        <div className="TextCard">
+          <div className="textcard">
+            <div className="lefttext text">
+              <h1 className="subheading">{heading}</h1>
+              <p>{text}</p>
             </div>
-            
-          )
+            <img src={img} />
+          </div>
+        </div>
+
+      )
     }
-    else{
-        return(
-            <div className="TextCard">
-                <div className="textcard">
-                  <img src={img}/>
-                  <div className="righttext text">
-                  <h1>{heading}</h1>
-                  <p>{text}</p>
-                </div>
-              </div>
+    else {
+      return (
+        <div className="TextCard">
+          <div className="textcard">
+            <img src={img} />
+            <div className="righttext text">
+              <h1 className="subheading">{heading}</h1>
+              <p>{text}</p>
             </div>
-            
-          )
+          </div>
+        </div>
+
+      )
     }
-    
+
   }
 }
