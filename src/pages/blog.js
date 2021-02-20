8b2c46e89fa8429a823ab1f878a69d5a549116fb
eilla1 <exu6056@gmail.com>
2021-02-19 21:01:39 -0800

    :sparkles: add blog cards/layout

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,16 +9,19 @@ export default function Home() {
     const data = useStaticQuery(
         graphql`
       query {
-        allContentfulBlogPost {
-            edges {
-              node {
-                author
-                datePublished(formatString: "MMMM Do, YYYY")
-                title
-                slug
-              }
+        allContentfulBlogPost(sort: {
+          fields: datePublished,
+          order: DESC
+        }) {
+          edges {
+            node {
+              author
+              datePublished(formatString: "MMMM Do, YYYY")
+              title
+              slug
             }
           }
+        }
       }
     `
     )
@@ -26,31 +29,27 @@ export default function Home() {
     return (
         <div>
             <Navbar />
+            <div className="featured-blog">
+                <img src="https://source.unsplash.com/random/1400x600" width="100%" />
+                <div className="bottom-left">
+                    <Link to="/blog/change-through-will-within"><h2>Change Through Will Within</h2></Link>
+                    <p>Olivia Guerra</p>
+                    <p>April 23rd, 2020</p>
+                </div>
+            </div>
             {data.allContentfulBlogPost.edges.map(edge => {
           return (
-              <>
-              <BlogCard image={Logo} title={edge.node.title} author={edge.node.author} date={edge.node.datePublished} likes="50" excerpt="excerpt" />
-              <div className="button">
-                <Link to={`/blog/${edge.node.slug}/`}>Read More</Link>
+            <>
+              <div className="blog-card-list">
+                <Link to={`/blog/${edge.node.slug}/`}></Link><BlogCard image={Logo} title={edge.node.title} author={edge.node.author} date={edge.node.datePublished} likes="50" excerpt="excerpt" />
               </div>
+              
+                
+              
               </>
           )
         })}
-            <div className="featured-blog">
-                <img src="" width="100%" />
-                <div className="bottom-left">
-                    <h2>hihi</h2>
-                    <p>Author</p>
-                    <p>Date</p>
-                </div>
-            </div>
-            <div class="blogcards">
-                <BlogCard image={Logo} title="title" author="author" date="date" likes="50" excerpt="excerpt" />
-                <BlogCard image={Logo} title="title2" author="author" date="date" likes="50" excerpt="excerpt" />
-            </div>
-        
-        
-
+            
             <Footer />
         </div>
     );
