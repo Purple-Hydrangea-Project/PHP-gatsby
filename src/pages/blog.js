4f60d01b71b2b1ac9316f6f8f2ed3cb58dd1d620
eilla1 <exu6056@gmail.com>
2021-04-17 17:22:58 -0700

    MAKE THE BLOG LIST RESPONSIVE
    ALSO REPLACED FEATURE BLOG WITH FEATURE COMPONENT

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,16 +1,14 @@
-import React from "react";
-import { useStaticQuery, graphql, Link } from "gatsby";
-import "../components/main.scss";
-import '../components/pages.scss';
-import { Footer, BlogCard, Navbar, Feature } from '../components';
-
+import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import "../components/main.scss"
+import "../components/pages.scss"
+import { Footer, BlogCard, Navbar, Feature } from "../components"
 
 export default function Home() {
-
-    const data = useStaticQuery(
-        graphql`
+  const data = useStaticQuery(
+    graphql`
       query {
-        allContentfulBlogPost(sort: {fields: datePublished, order: DESC}) {
+        allContentfulBlogPost(sort: { fields: datePublished, order: DESC }) {
           edges {
             node {
               author
@@ -30,25 +28,43 @@ export default function Home() {
         }
       }
     `
-    )
+  )
 
-    return (
-        <div className="blogListPage">
-            <Navbar />
-            <div>
-                <Feature image="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" title="Change through Will Within" author="Olivia Guerra" date="April 23, 2020" likes="10" />
-            </div>
-            {data.allContentfulBlogPost.edges.map(edge => {
+  return (
+    <div className="blogListPage">
+      <Navbar />
+      <div>
+        <Feature
+          image="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg"
+          title="Change through Will Within"
+          author="Olivia Guerra"
+          date="April 23, 2020"
+          likes="10"
+        />
+      </div>
+      <div className="blogCardList">
+        {data.allContentfulBlogPost.edges.map(edge => {
           return (
             <>
-              <div className="blogCardList">
-                <Link className="blogCardLink" to={`/blog/${edge.node.slug}/`}><BlogCard className="blogCardCard" image={edge.node.photo.file.url} title={edge.node.title} author={edge.node.author} date={edge.node.datePublished} likes="50" excerpt={edge.node.excerpt.excerpt} /></Link>
+              <div className="f">
+                <Link className="blogCardLink" to={`/blog/${edge.node.slug}/`}>
+                  <BlogCard
+                    className="blogCardCard"
+                    image={edge.node.photo.file.url}
+                    title={edge.node.title}
+                    author={edge.node.author}
+                    date={edge.node.datePublished}
+                    likes="50"
+                    excerpt={edge.node.excerpt.excerpt}
+                  />
+                </Link>
               </div>
             </>
           )
         })}
-            
-            <Footer />
-        </div>
-    );
+      </div>
+
+      <Footer />
+    </div>
+  )
 }
